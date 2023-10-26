469119a6593998468742e3ec825f53f584314e5e
TSVTLN <tsvtln@abv.bg>
2023-10-26 19:14:47 +0300

    Update 04_cup.py

--- 04_ex_first_steps_OOP/04_cup.py
+++ 04_ex_first_steps_OOP/04_cup.py
@@ -12,8 +12,32 @@ class Cup:
 
 
 """test code"""
+
+
 # cup = Cup(100, 50)
 # print(cup.status())
 # cup.fill(40)
 # cup.fill(20)
 # print(cup.status())
+
+
+# dynamic class
+# def init_cup(self, size: int, quantity: int):
+#     self.size = size
+#     self.quantity = quantity
+#
+#
+# def fill_cup(self, quantity):
+#     if quantity + self.quantity <= self.size:
+#         self.quantity += quantity
+#
+#
+# def status_cup(self):
+#     return self.size - self.quantity
+#
+#
+# Cup = type('Cup', (), {
+#     '__init__': init_cup,
+#     'fill': fill_cup,
+#     'status': status_cup
+# })
