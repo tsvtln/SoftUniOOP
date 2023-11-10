39d0091b33b4be8d925d7ea1602c077eca8e158a
TSVTLN <tsvtln@abv.bg>
2023-11-10 17:38:14 +0200

    restaurant project

---
+++ 10_ex_encapsulation/04_restaurant/project/beverage/coffee.py
@@ -0,0 +1,15 @@
+from project.beverage.hot_beverage import HotBeverage
+
+
+class Coffee(HotBeverage):
+    MILLILITERS = 50
+    PRICE = 3.50
+
+    def __init__(self, name: str, caffeine: float):
+        super().__init__(name, self.PRICE, self.MILLILITERS)
+        self.__caffeine = caffeine
+
+    @property
+    def caffeine(self):
+        return self.__caffeine
+
---
+++ 10_ex_encapsulation/04_restaurant/project/food/cake.py
@@ -0,0 +1,11 @@
+from project.food.dessert import Dessert
+
+
+class Cake(Dessert):
+    GRAMS = 250
+    CALORIES = 1000
+    PRICE = 5
+
+    def __init__(self, name):
+        #  price: float, grams: float, calories: float):
+        super().__init__(name, self.PRICE, self.GRAMS, self.CALORIES)
