19b0839852227750ab1b0f25fb85b214e52bccd8
TSVTLN <tsvtln@abv.bg>
2023-11-23 16:05:18 +0200

    Update 03_bold_italic_underline.py

--- 20_decorators/03_bold_italic_underline.py
+++ 20_decorators/03_bold_italic_underline.py
@@ -1,25 +1,49 @@
-def make_bold(function):
-    def wrapper(*args, **kwargs):
-        values = function(*args, **kwargs)
-        return f"<b>{values}</b>"
+# def make_bold(function):
+#     def wrapper(*args, **kwargs):
+#         values = function(*args, **kwargs)
+#         return f"<b>{values}</b>"
+#
+#     return wrapper
+#
+#
+# def make_italic(function):
+#     def wrapper(*args, **kwargs):
+#         values = function(*args, **kwargs)
+#         return f"<i>{values}</i>"
+#
+#     return wrapper
+#
+#
+# def make_underline(function):
+#     def wrapper(*args, **kwargs):
+#         values = function(*args, **kwargs)
+#         return f"<u>{values}</u>"
+#
+#     return wrapper
+#
+#
+# @make_bold
+# @make_italic
+# @make_underline
+# def greet(name):
+#     return f"Hello, {name}"
+#
+#
+# print(greet("Peter"))
 
-    return wrapper
+
+def make_format(format_symbol):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            values = function(*args, **kwargs)
+            return f"<{format_symbol}>{values}</{format_symbol}>"
+        return wrapper
+    return decorator
 
 
-def make_italic(function):
-    def wrapper(*args, **kwargs):
-        values = function(*args, **kwargs)
-        return f"<i>{values}</i>"
-
-    return wrapper
-
-
-def make_underline(function):
-    def wrapper(*args, **kwargs):
-        values = function(*args, **kwargs)
-        return f"<u>{values}</u>"
-
-    return wrapper
+make_bold = make_format('b')
+make_italic = make_format('i')
+make_underline = make_format('u')
 
 
 @make_bold
