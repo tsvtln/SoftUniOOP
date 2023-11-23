96466dba3ac4fb192b212ee32bd435d6dbe42425
TSVTLN <tsvtln@abv.bg>
2023-11-23 13:31:20 +0200

    Create 03_even_numbers.py

---
+++ 20_decorators/Lab/03_even_numbers.py
@@ -0,0 +1,16 @@
+def even_numbers(function):
+    # @wrapper(function)
+    def wrapper(numbers):
+
+        lst = []
+        for number in numbers:
+            if number % 2 == 0:
+                lst.append(number)
+        return lst
+
+    return wrapper
+
+@even_numbers
+def get_numbers(numbers):
+    return numbers
+print(get_numbers([1, 2, 3, 4, 5]))
