151f8578efcaf011b8d338cbd048e56ba06e0014
TSVTLN <112159858+tsvtln@users.noreply.github.com>
2023-11-23 18:24:28 +0200

    Update 03_bold_italic_underline.py

--- 20_decorators/03_bold_italic_underline.py
+++ 20_decorators/03_bold_italic_underline.py
@@ -1,37 +1,3 @@
-# def make_bold(function):
-#     def wrapper(*args, **kwargs):
-#         values = function(*args, **kwargs)
-#         return f"<b>{values}</b>"
-#
-#     return wrapper
-#
-#
-# def make_italic(function):
-#     def wrapper(*args, **kwargs):
-#         values = function(*args, **kwargs)
-#         return f"<i>{values}</i>"
-#
-#     return wrapper
-#
-#
-# def make_underline(function):
-#     def wrapper(*args, **kwargs):
-#         values = function(*args, **kwargs)
-#         return f"<u>{values}</u>"
-#
-#     return wrapper
-#
-#
-# @make_bold
-# @make_italic
-# @make_underline
-# def greet(name):
-#     return f"Hello, {name}"
-#
-#
-# print(greet("Peter"))
-
-
 def make_format(format_symbol):
     def decorator(function):
         def wrapper(*args, **kwargs):
