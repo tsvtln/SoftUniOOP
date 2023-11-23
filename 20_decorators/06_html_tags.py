bdb764a72daa346e6008d48f80d9d80032e266fa
TSVTLN <tsvtln@abv.bg>
2023-11-23 16:55:19 +0200

    Update 06_html_tags.py

---
+++ 20_decorators/06_html_tags.py
@@ -0,0 +1,28 @@
+from functools import wraps
+
+
+def tags(tag):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args):
+            return f"<{tag}>{func(*args)}</{tag}>"
+
+        return wrapper
+
+    return decorator
+
+
+@tags('p')
+def join_strings(*args):
+    return "".join(args)
+
+
+print(join_strings("Hello", " you!"))
+
+
+@tags('h1')
+def to_upper(text):
+    return text.upper()
+
+
+print(to_upper('hello'))
