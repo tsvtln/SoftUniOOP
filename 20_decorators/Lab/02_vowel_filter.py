318964f0df9b8dd6d1c3a6e121cdfd590b49a8f1
TSVTLN <tsvtln@abv.bg>
2023-11-23 12:40:07 +0200

    Create 02_vowel_filter.py

---
+++ 20_decorators/Lab/02_vowel_filter.py
@@ -0,0 +1,19 @@
+def vowel_filter(function):
+    def wrapper():
+
+        letters = function()
+        tr = []
+        for letter in letters:
+            if letter in ['a', 'e', 'i', 'o', 'u', 'y']:
+                tr.append(letter)
+        return tr
+
+    return wrapper
+
+
+@vowel_filter
+def get_letters():
+    return ["a", "b", "c", "d", "e"]
+
+
+print(get_letters())
