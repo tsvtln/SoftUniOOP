bf117f944b8e04d656ec7d7f088a4116da45471a
TSVTLN <tsvtln@abv.bg>
2023-11-23 13:46:27 +0200

    u

--- 20_decorators/Lab/03_even_numbers.py
+++ 20_decorators/Lab/03_even_numbers.py
@@ -4,13 +4,11 @@ from functools import wraps
 def even_numbers(function):
     @wraps(function)
     def wrapper(numbers):
-
         lst = []
         for number in numbers:
             if number % 2 == 0:
                 lst.append(number)
         return lst
-
     return wrapper
 
 
