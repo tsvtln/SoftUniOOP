bac7eb743511eb5849d942dea0c01c05be8869db
TSVTLN <112159858+tsvtln@users.noreply.github.com>
2023-11-23 18:53:30 +0200

    Update 05_cache.py

--- 20_decorators/05_cache.py
+++ 20_decorators/05_cache.py
@@ -11,8 +11,7 @@ def cache(function):
 def fibonacci(n):
     if n < 2:
         return n
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 fibonacci(3)
