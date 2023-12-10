65eca573af69a5de3a6a327280e8bd99c4814f0a
tsvtln <tsvtln@abv.bg>
2023-12-10 14:35:02 +0200

    Update nautical_catch_challenge_app.py

--- regular_exam/project/nautical_catch_challenge_app.py
+++ regular_exam/project/nautical_catch_challenge_app.py
@@ -131,7 +131,8 @@ class NauticalCatchChallengeApp:
         return '\n'.join(to_return)
 
     def competition_statistics(self):
-        sorted_divers = sorted([diver for diver in self.divers if not diver.has_health_issue and not diver.oxygen_level == 0],
+        sorted_divers = sorted([diver for diver in self.divers
+                                if not diver.has_health_issue and not diver.oxygen_level == 0],
                                key=lambda x: (-x.competition_points, -len(x.catch), x.name))
 
         result = [f"**Nautical Catch Challenge Statistics**"]
