0b55166de33242fa7be52705db1a689522502fad
tsvtln <tsvtln@abv.bg>
2023-12-10 11:47:33 +0200

    Update scuba_diver.py

--- regular_exam/project/divers/scuba_diver.py
+++ regular_exam/project/divers/scuba_diver.py
@@ -6,14 +6,6 @@ class ScubaDiver(BaseDiver):
     def __init__(self, name: str):
         super().__init__(name, 540)
 
-    # ORIG
-    # def miss(self, time_to_catch: int):
-    #     self.oxygen_level -= 0.30 * time_to_catch
-    #     if self.oxygen_level < 0:
-    #         self.oxygen_level = 0
-    #     else:
-    #         self.oxygen_level = round(self.oxygen_level)
-
     def miss(self, time_to_catch: int):
         self.oxygen_level -= 0.30 * time_to_catch
         if self.oxygen_level < 0:
