7db4225bdedd9e88f7cc52606206603b709ce74b
tsvtln <tsvtln@abv.bg>
2023-12-16 15:29:04 +0200

    150/150

--- OOP_exam_5august2023/project/bank_app.py
+++ OOP_exam_5august2023/project/bank_app.py
@@ -45,34 +45,58 @@ class BankApp:
 
     def grant_loan(self, loan_type: str, client_id: str):
         # find if client is student or adult and check if the loan type is correct
-        client_object = self.__object_grep(client_id, "Student")
-        if client_object is not None:
-            if loan_type != 'StudentLoan':
-                raise Exception("Inappropriate loan type!")
-            loan = next((obj for obj in getattr(self, self.loans) if obj == StudentLoan), None)
+        # client_object = self.__object_grep(client_id, "Student")
+        client_object = [c for c in self.clients if c.client_id == client_id][0]
+        if (loan_type == 'StudentLoan' and client_object.__class__.__name__ == 'Student') or \
+                (loan_type == "MortgageLoan" and client_object.__class__.__name__ == 'Adult'):
+            loan = [l for l in self.loans if l.__class__.__name__ == loan_type][0]
+            self.loans.remove(loan)
             client_object.loans.append(loan)
-            self.loans.remove(loan)
-            return f"Successfully granted {loan_type} to {client_object.client_name} with ID {client_object.client_id}."
-
-        if client_object is None:
-            client_object_2 = self.__object_grep(client_id, "Adult")
-            if loan_type != 'MortgageLoan':
-                raise Exception("Inappropriate loan type!")
-            loan = next((obj for obj in getattr(self, self.loans) if
-                         obj == MortgageLoan), None)
-            client_object_2.loans.append(loan)
-            self.loans.remove(loan)
-            return (f"Successfully granted {loan_type} to {client_object_2.client_name} with ID "
-                    f"{client_object_2.client_id}.")
+            return f"Successfully granted {loan_type} to {client_object.name} with ID {client_id}."
+        else:
+            raise Exception("Inappropriate loan type!")
 
     def remove_client(self, client_id: str):
-        pass
+        try:
+            client_object = [c for c in self.clients if c.client_id == client_id][0]
+        except IndexError:
+            raise Exception("No such client!")
+        if client_object.loans:
+            raise Exception("The client has loans! Removal is impossible!")
+        self.clients.remove(client_object)
+        return f"Successfully removed {client_object.name} with ID {client_id}."
 
     def increase_loan_interest(self, loan_type: str):
-        pass
+        cnt = 0
+        for loan in self.loans:
+            if loan.__class__.__name__ == loan_type:
+                loan.increase_interest_rate()
+                cnt += 1
+        return f"Successfully changed {cnt} loans."
 
     def increase_clients_interest(self, min_rate: float):
-        pass
+        cnt = 0
+        for client in self.clients:
+            if client.interest < min_rate:
+                client.increase_clients_interest()
+                cnt += 1
+        return f"Number of clients affected: {cnt}."
 
     def get_statistics(self):
-        pass
+        total_clients_income = sum([c.income for c in self.clients])
+        loans_count_granted_to_clients = sum([len(c.loans) for c in self.clients])
+        granted_sum = sum([l.amount for c in self.clients for l in c.loans])
+        not_granted_sum = sum([l.amount for l in self.loans])
+
+        try:
+            avg_client_interest_rate = sum([c.interest for c in self.clients]) / len(self.clients)
+        except ZeroDivisionError:
+            avg_client_interest_rate = 0
+
+        result = f"Active Clients: {len(self.clients)}\n"
+        result += f"Total Income: {total_clients_income:.2f}\n"
+        result += f"Granted Loans: {loans_count_granted_to_clients}, Total Sum: {granted_sum:.2f}\n"
+        result += f"Available Loans: {len(self.loans)}, Total Sum: {not_granted_sum:.2f}\n"
+        result += f"Average Client Interest Rate: {avg_client_interest_rate:.2f}"
+
+        return result
--- OOP_exam_5august2023/project/clients/student.py
+++ OOP_exam_5august2023/project/clients/student.py
@@ -2,7 +2,6 @@ from project.clients.base_client import BaseClient
 
 
 class Student(BaseClient):
-
     def __init__(self, name: str, client_id: str, income: float):
         super().__init__(name, client_id, income, 2.0)
 
